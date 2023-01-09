077692673b11198674d246f42076a2441bc02439
jthomas010323 <114627890+jthomas010323@users.noreply.github.com>
2023-01-08 21:52:23 -0500

    Time: 126 ms (95.95%), Space: 14.6 MB (65.09%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         i = 0
         for j in range(len(nums)):
-            if (nums[j] != 0):
+            if nums[j] != 0:
                 nums[i], nums[j] = nums[j], nums[i]
                 i += 1
                 
