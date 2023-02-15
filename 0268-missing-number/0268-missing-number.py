4ae289202cd7c50006c8962792441177c76500af
jthomas010323 <114627890+jthomas010323@users.noreply.github.com>
2023-02-15 08:55:21 -0500

    Time: 105 ms (69.36%), Space: 14.6 MB (74.22%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        j=len(nums)
+        summ=j*(j+1)//2
+        for i in nums:
+            summ-=i
+        return summ
+        
+            
+                
